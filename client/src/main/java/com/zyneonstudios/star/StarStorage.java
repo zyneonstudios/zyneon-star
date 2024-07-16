5bdbd577626149aed0b6181ad8999da3d9f0e31f
nerotvlive <daniel.niesmann@outlook.com>
2024-07-17 01:22:17 +0200

    Fix editor layout

--- client/src/main/java/com/zyneonstudios/star/StarStorage.java
+++ client/src/main/java/com/zyneonstudios/star/StarStorage.java
@@ -2,5 +2,5 @@ package com.zyneonstudios.star;
 
 public class StarStorage {
 
-    public static String starUrlBase = "https://star.zyneonstudios.com/";
+    public static String starUrlBase = "https://zyneonstudios.github.io/zyneon-star/";
 }
