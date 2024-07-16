de6d5032f61fbfef2c16b51c3545715c987a0927
nerotvlive <daniel.niesmann@outlook.com>
2024-07-16 21:53:26 +0200

    Update article create page

--- assets/js/create-article.js
+++ assets/js/create-article.js
@@ -1,6 +1,17 @@
+let title = "";
+let id= "";
+
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     if(urlParams.has("id")) {
         document.querySelector("iframe").src = "../templates/editor.html?id="+urlParams.get("id");
     }
 });
+
+function setTitle() {
+    title = document.getElementById("article-title").value;
+    if(title) {
+        id = title.replaceAll(" ", "-").replace(/[^a-z0-9-_]/gi, '').toLowerCase();
+        document.querySelector("iframe").src = "../templates/editor.html?id="+id;
+    }
+}
--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,12 +1,34 @@
 let id = generateUUIDv4();
+let back = "..";
+let editor;
+let fullscreen = false;
+
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     if(urlParams.has("id")) {
         id = urlParams.get("id");
     }
+    if(urlParams.has("back")) {
+        back = urlParams.get("back");
+        if(back==="reload") {
+            back = location.href;
+            if(back.includes("?")) {
+                back = back + "&id="+id;
+            } else {
+                back = back + "?id="+id;
+            }
+            const a = document.querySelector("#exit").querySelector("a");
+            a.innerHTML = "<i class='bx bx-refresh'></i>";
+            if(a.classList.contains("red")) {
+                a.classList.remove("red");
+            }
+            if(!a.classList.contains("blue")) {
+                a.classList.add("blue");
+            }
+        }
+    }
     document.getElementById("md-editor").id = "editor_"+id;
-
-    new SimpleMDE(
+    editor = new SimpleMDE(
         {
             autoDownloadFontAwesome: true,
             autofocus: true,
@@ -30,4 +52,35 @@ document.addEventListener('DOMContentLoaded', function() {
             spellChecker: false
         }
     );
+
+    setInterval(function() {
+        if(editor.isFullscreenActive()!==fullscreen) {
+            const a = document.getElementById("exit").querySelector("a");
+            const r = document.getElementById("r");
+            const l = document.getElementById("l");
+            if(editor.isFullscreenActive()) {
+                fullscreen = true;
+                if(!a.classList.contains("active")) {
+                    a.classList.add("active");
+                }
+                if(!l.classList.contains("active")) {
+                    l.classList.add("active");
+                }
+                if(!r.classList.contains("active")) {
+                    r.classList.add("active");
+                }
+            } else {
+                fullscreen = false;
+                if(a.classList.contains("active")) {
+                    a.classList.remove("active");
+                }
+                if(l.classList.contains("active")) {
+                    l.classList.remove("active");
+                }
+                if(r.classList.contains("active")) {
+                    r.classList.remove("active");
+                }
+            }
+        }
+    }, 150);
 });
