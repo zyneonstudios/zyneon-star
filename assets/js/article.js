543c6bc10131209a2635560e6eb39d882c6a212a
nerotvlive <daniel.niesmann@outlook.com>
2024-07-16 18:45:56 +0200

    Add general setArticle function

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,31 +1,64 @@
+function setArticle(author,backgroundUrl,buttons,datePublished,markdown,title) {
+    if(author) {
+        setArticleAuthor(author);
+    }
+    if(backgroundUrl) {
+        setArticleBackgroundUrl(backgroundUrl);
+    }
+    if(buttons) {
+        setArticleButtons(buttons);
+    }
+    if(datePublished) {
+        setArticleDatePublished(datePublished);
+    }
+    if(markdown) {
+        setArticleMarkdown(markdown);
+    }
+    if(title) {
+        setArticleTitle(title);
+    }
+}
+
 function setArticleAuthor(newAuthor) {
-    const author = document.getElementById("article-author");
-    author.innerText = newAuthor;
+    if(newAuthor) {
+        const author = document.getElementById("article-author");
+        author.innerText = newAuthor;
+    }
 }
 
 function setArticleBackgroundUrl(newBackgroundUrl) {
-    const background = document.getElementById("header");
-    background.style.background = "linear-gradient(to top, var(--background), transparent), url('"+newBackgroundUrl+"') no-repeat center";
+    if(newBackgroundUrl) {
+        const background = document.getElementById("header");
+        background.style.background = "linear-gradient(to top, var(--background), transparent), url('"+newBackgroundUrl+"') no-repeat center";
+    }
 }
 
 function setArticleButtons(newButtons) {
-    const buttons = document.getElementById("article-buttons");
-    buttons.innerHTML = "<h6><a class=\"button\" href=\"..\"><i class='bx bx-left-arrow-alt'></i><span class=\".back\">Back</span></a></h6>"+newButtons;
+    if(newButtons) {
+        const buttons = document.getElementById("article-buttons");
+        buttons.innerHTML = "<h6><a class=\"button\" href=\"..\"><i class='bx bx-left-arrow-alt'></i><span class=\".back\">Back</span></a></h6>"+newButtons;
+    }
 }
 
 function setArticleDatePublished(newDate) {
-    const datePublished = document.getElementById("article-published");
-    datePublished.innerText = newDate;
+    if(newDate) {
+        const datePublished = document.getElementById("article-published");
+        datePublished.innerText = newDate;
+    }
 }
 
 function setArticleMarkdown(newMarkdown) {
-    const markdown = document.getElementById("article");
-    markdown.innerHTML = "<md-block id='article-markdown'>"+newMarkdown+"</md-block>";
+    if(newMarkdown) {
+        const markdown = document.getElementById("article");
+        markdown.innerHTML = "<md-block id='article-markdown'>"+newMarkdown+"</md-block>";
+    }
 }
 
 function setArticleTitle(newTitle) {
-    const title = document.getElementById("article-title");
-    title.innerText = newTitle;
+    if(newTitle) {
+        const title = document.getElementById("article-title");
+        title.innerText = newTitle;
+    }
 }
 
 function init() {
